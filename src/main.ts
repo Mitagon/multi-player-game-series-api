7a4cb71fae1bfbfabc6324dac02c8766eeb0d010
ikenjokudc <ikenjokudc@gmail.com>
2022-09-07 22:24:16 +0100

    Reduced system downtime by adding utilizing cluster module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const cluster = require('cluster');
+const os = require('os');
+const PORT = process.env.PORT || 3000;
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+const cpus = os.cpus().length;
+
+if (cluster.isMaster) {
+  for (let i = 0; i < cpus; i++) {
+    cluster.fork()
+  };
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died...`);
+    cluster.fork();
+  })
+
+} else {
+  async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(PORT)
+      .then(() => {
+        console.log(`Server is runnint on port ${PORT} || ==>>> PID: ${process.pid}`)
+      })
+  }
+  bootstrap();
 }
-bootstrap();
+
+
